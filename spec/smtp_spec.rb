5b28041c56a0ef2eec5c161193eb01f66259b4e9
Paul Annesley <paul@annesley.cc>
2013-10-15 12:36:53 -0700

    smtp_spec brevity.

--- spec/smtp_spec.rb
+++ spec/smtp_spec.rb
@@ -21,14 +21,12 @@ describe "SMTP configuration" do
       env["POSTMARK_SMTP_SERVER"] = "postmark.example.org"
       env["POSTMARK_API_KEY"] = "b6ebafbec9a31661f6247f21ff4a68d9"
     }
-    it "has correct address" do
-      expect(config[:address]).to eq("postmark.example.org")
-    end
-    it "has correct username" do
-      expect(config[:username]).to eq("b6ebafbec9a31661f6247f21ff4a68d9")
-    end
-    it "has correct password" do
-      expect(config[:password]).to eq("b6ebafbec9a31661f6247f21ff4a68d9")
+    it "sets address, username, password" do
+      expect(config.to_h).to eq(
+        address: "postmark.example.org",
+        username: "b6ebafbec9a31661f6247f21ff4a68d9",
+        password: "b6ebafbec9a31661f6247f21ff4a68d9",
+      )
     end
   end
 
