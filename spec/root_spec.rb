fb2f620012f08f6668fcf41556801aa54dcf117c
Paul Annesley <paul@annesley.cc>
2013-10-16 11:07:18 -0700

    root_spec has #to_h example with lots of ENV vars.

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -51,4 +51,59 @@ describe Envconfig::Root do
 
   end
 
+  context "with all of the things in ENV" do
+    let(:env) do
+      # Note: actual precedence is defined in the service classes,
+      # generally based on alphabetical sorting.
+      # This example ENV intentionally mixes up the precedence ordering.
+      # e.g. the Redis services match their actual precedence,
+      # but the SMTP services reverse their actual precedence.
+      {
+        "DATABASE_URL" => "postgres://u:p@db.example.org:1234/db",
+        "MEMCACHIER_SERVERS" => "mc.example.org:11211",
+        "MEMCACHIER_USERNAME" => "mcuser",
+        "MEMCACHIER_PASSWORD" => "mcpass",
+        "OPENREDIS_URL" => "redis://:orpass@or.example.org:2345",
+        "REDISTOGO_URL" => "redis://:rtgpass@rtg.example.org:3456",
+        "SENDGRID_USERNAME" => "sguser",
+        "SENDGRID_PASSWORD" => "sgpass",
+        "POSTMARK_SMTP_SERVER" => "pm.example.org",
+        "POSTMARK_API_KEY" => "pmkey",
+      }
+    end
+    it "has a giant #to_h" do
+      expect(root.to_h).to eq(
+        database: {
+          url: "postgres://u:p@db.example.org:1234/db",
+          adapter: "postgresql",
+          database: "db",
+          username: "u",
+          password: "p",
+          host: "db.example.org",
+          port: 1234
+        },
+        memcached: {
+          servers: "mc.example.org:11211",
+          username: "mcuser",
+          password: "mcpass",
+          server_strings: ["mc.example.org:11211"]
+        },
+        redis: {
+          url: "redis://:orpass@or.example.org:2345",
+          host: "or.example.org",
+          port: 2345,
+          user: "",
+          password: "orpass"
+        },
+        smtp: {
+          port: "25",
+          authentication: :plain,
+          address: "pm.example.org",
+          user_name: "pmkey",
+          password: "pmkey"
+        }
+      )
+    end
+  end
+
 end
